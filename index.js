98c798ff0aca5e04c0bdc9ccec289f33aa7c901a
NomadDaniel <reachingdaniel@gmail.com>
2020-03-26 16:05:34 -0400

    10 of 46, Person complete

--- index.js
+++ index.js
@@ -43,8 +43,28 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+  }
+  eat(someFood){
+  if(this.stomach.length < 10){
+    this.stomach.push(someFood);
+    }
+  }
+  poop(){
+    if(this.stomach = []){
+    }
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;    
+  }
+};
 
-}
+const Billy = new Person({name: "billy", age: 30})
+console.log(Billy)
+
 
 /*
   TASK 2
